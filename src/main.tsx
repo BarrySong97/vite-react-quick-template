9b70462f249bba724d8d2af8e9ec94ab606e603b
songtianjian <songtianjian@sobey.com>
2022-05-06 13:54:31 +0800

    feat(unocss): add unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import '@unocss/reset/normalize.css';
+import '@unocss/reset/eric-meyer.css';
+import '@unocss/reset/tailwind.css';
+import 'uno.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
